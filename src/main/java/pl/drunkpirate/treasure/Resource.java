ea5ce0ce2a81d2590d2093b3b218f3844ffe9a44
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2011-06-06 18:28:42 +0200

    Initial commit

---
+++ src/main/java/pl/drunkpirate/treasure/Resource.java
@@ -0,0 +1,8 @@
+package pl.drunkpirate.treasure;
+
+import java.util.logging.Logger;
+
+public class Resource {
+    @SuppressWarnings("unused")
+    private static final Logger LOGGER = Logger.getLogger(Resource.class.getName());
+}
---
+++ src/test/java/pl/drunkpirate/treasure/ResourceTest.java
@@ -0,0 +1,20 @@
+package pl.drunkpirate.treasure;
+
+
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class ResourceTest {
+    @SuppressWarnings("unused")
+    private static final Logger LOGGER = Logger.getLogger(ResourceTest.class.getName());
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
